9a5b98cc324c72e3793bd54b34fa0c6de35e9aac
miggo <miggo@charizard.fritz.box>
2016-09-15 20:22:06 +0200

    added remaining playtime indicator

--- music.bash
+++ music.bash
@@ -4,6 +4,25 @@ title=$(qdbus org.mpris.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer
 
 playback=$(qdbus org.mpris.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus)
 
+length=$(qdbus org.mpris.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep length | grep -oP ": (.)*$")
+
+position=$(qdbus org.mpris.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Position )
+
+left="$((${length:2} - $position))"
+min="$(($left / 60000000 ))"
+sec="$(($left / 1000000 ))"
+sec="$(($sec - ($min * 60)))"
+minZero=""
+secZero=""
+
+if [ ${min} -lt 10 ] ; then
+	minZero="0";
+fi
+
+if [ ${sec} -lt 10 ] ; then
+	secZero="0";
+fi
+
 if [ $playback == 'Playing' ] ; then
 	playback="⯈";
 #	playback="⏵";
@@ -16,5 +35,5 @@ if [ $playback == 'Stopped' ] ; then
 #	echo ◼ ;
 	echo ⏹ ;
 else
-	echo $playback ${artist:2} - ${title:2} 🤘
+	echo $playback ${artist:2} - ${title:2}  -${minZero}${min}:${secZero}${sec} 🤘
 fi
